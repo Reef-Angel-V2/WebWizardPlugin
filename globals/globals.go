655e5858ca20188238d330da9779b2fd67d78c47
brennyn21 <84816265+brennyn21@users.noreply.github.com>
2024-07-03 16:41:18 -0600

    Update globals.go
    Change public Key

--- globals/globals.go
+++ globals/globals.go
@@ -18,5 +18,7 @@ package globals
 // DefaultIndexURL is the default index url
 var (
 	// SignatureKey is the public key used to verify commands and url sent by the builder
-	SignatureKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvc0yZr1yUSen7qmE3cxF\nIE12rCksDnqR+Hp7o0nGi9123eCSFcJ7CkIRC8F+8JMhgI3zNqn4cUEn47I3RKD1\nZChPUCMiJCvbLbloxfdJrUi7gcSgUXrlKQStOKF5Iz7xv1M4XOP3JtjXLGo3EnJ1\npFgdWTOyoSrA8/w1rck4c/ISXZSinVAggPxmLwVEAAln6Itj6giIZHKvA2fL2o8z\nCeK057Lu8X6u2CG8tRWSQzVoKIQw/PKK6CNXCAy8vo4EkXudRutnEYHEJlPkVgPn\n2qP06GI+I+9zKE37iqj0k1/wFaCVXHXIvn06YrmjQw6I0dDj/60Wvi500FuRVpn9\ntwIDAQAB\n-----END PUBLIC KEY-----"
+	//SignatureKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvc0yZr1yUSen7qmE3cxF\nIE12rCksDnqR+Hp7o0nGi9123eCSFcJ7CkIRC8F+8JMhgI3zNqn4cUEn47I3RKD1\nZChPUCMiJCvbLbloxfdJrUi7gcSgUXrlKQStOKF5Iz7xv1M4XOP3JtjXLGo3EnJ1\npFgdWTOyoSrA8/w1rck4c/ISXZSinVAggPxmLwVEAAln6Itj6giIZHKvA2fL2o8z\nCeK057Lu8X6u2CG8tRWSQzVoKIQw/PKK6CNXCAy8vo4EkXudRutnEYHEJlPkVgPn\n2qP06GI+I+9zKE37iqj0k1/wFaCVXHXIvn06YrmjQw6I0dDj/60Wvi500FuRVpn9\ntwIDAQAB\n-----END PUBLIC KEY-----"
+	// SignatureKey is the public key used to verify commands and url sent by the builder
+	SignatureKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAky7SolvcpeKd2AWiAhGk\nZirHIrE5phD4aMMqnUDYXjS8rfOOqZ8Ov+14ptA7JQGKpF1gNZUW8Oe8eJoruilX\nM9IMEUHcCwxbX+rNTDk9XlD3/0wukl3DLq/EFudcc4vLuSJEeEdjDL7eYHRzixtH\nnJ/9Vq1/de3E0eL/PPb4VPr3IAJ3O83ze0ctRyPQS2fxheJy9mNZt5otChi7l95f\nllm5fikFyWQ1nhnN6A/1x1SggR2kSOCgs8fmbPukTBCGM9zzvBUGQw/XlMO1h3/I\n2t8HpgAkY0srhxKVXMcKkHmk4iX258VY2bEdNMVluSqN/3iadMeYdG7hNQ1fJ2nH\nqwIDAQAB\n-----END PUBLIC KEY-----"
 )
